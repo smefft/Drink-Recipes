5d1a0aae97b7fa2e568a5761212f9157b29b07d2
Sandra Effert <sandra.m.effert@gmail.com>
2023-10-05 22:41:10 +0200

    REFORMAT drinks_driver

--- Drinks.py
+++ Drinks.py
@@ -1,29 +1,32 @@
 import requests
 
 class Drinks:
-    def __init__(self, spirit):
-        # Access API of chosen spirit
-        self.spirit = spirit
-        self.response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={}'.format(spirit))
-        self.drinklist = self.response.json()
-
-    def drinks(self):
-        # split data into separate drinks - containing drink id, name, and jpg
-        self.drink_info = []
+    def __init__(self):
         self.drink_names = []
+        self.drink_infos = []
 
-        for drink_info in self.drinklist['drinks']:
-            self.drink_info.append(drink_info)
-
-        # isolate drink name
-        for drink_name in self.drink_info:
-            for key in drink_name:
-                if key == 'strDrink':
-                    self.drink_names.append(drink_name[key])
-
-        # print all drink names
-        print('\nDrink Options:')
-        i=1
-        for drink in self.drink_names:
-            print("{}) ".format(i), drink)
-            i+=1
+    def populate_drinklist(self, spirit):
+        self.spirit = spirit
+        response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={}'.format(spirit))
+        json_data: dict = response.json()
+        for drink_info in json_data["drinks"]:
+            self.drink_infos.append(drink_info)
+            self.drink_names.append(drink_info["strDrink"])
+    
+    def print_drinklist(self):
+        print(f"Drinks with {self.spirit}:")
+        for index, drink_name in enumerate(self.drink_names, 1):
+            print(f"{index}: {drink_name}")
+    
+    def pick_drink_with_index(self, index:int):
+        try:
+            drink = self.drink_names[index-1]
+            return drink
+        except IndexError:
+            print(f"Pick a number between 1 and {len(self.drink_names)}")
+            return None
+    
+    def get_drink_id(self, drink):
+        for drink_info in self.drink_infos:
+            if drink_info["strDrink"] == drink:
+                return drink_info["idDrink"]
--- drinks_driver.py
+++ drinks_driver.py
@@ -2,26 +2,8 @@ import json
 from Drinks import Drinks
 from Recipe import Recipe
 
-
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-def drink_id(drink_choice, drink_list):
-    info = {}
-    # find chosen drink in original list of drinks with user spirit
-    for drink_info in drink_list:
-        for key in drink_info:
-            if drink_info[key]== drink_choice:
-                info.update(dict(drink_info))
-    # isolate drink ID
-    for key in info:
-        if key == 'idDrink':
-            drink_id = info['idDrink']
-            return drink_id
-
-print("\n---Welcome to your personal drink generator---\n")
-print("Spirits to choose from:\n"
+def print_list_of_spirits() -> None:
+    print("Spirits to choose from:\n"
       "    Gin\n"
       "    Tequila\n"
       "    Vodka\n"
@@ -29,28 +11,52 @@ print("Spirits to choose from:\n"
       "    Whiskey\n"
       "    Brandy\n"
       "    Bourbon\n")
-spirits = ['gin', 'tequila', 'vodka', 'rum', 'whiskey','brandy', 'bourbon']
-# Get user's spirit of choice
-spirit = input('Choose your spirit:\n').strip().lower()
+    
+def set_user_spirit() -> str:
+    spirit = ""
+    spirits = ['gin', 'tequila', 'vodka', 'rum', 'whiskey','brandy', 'bourbon']
+    while spirit not in spirits:
+        spirit = input('Choose your spirit:\n').strip().lower()
+        if spirit not in spirits:
+            print(f"You picked {spirit}, which is not on the list. Please pick a spirit from the list.")
+    return spirit
 
-# Check if spirit is actually a spirit
-if spirit in spirits:
-    user_drinks = Drinks(spirit)
-    user_drinks.drinks() # print list of drinks with chosen spirits
-    chosen_drink = input('\nPlease choose:\n').strip()
-    # have user choose a drink from the list
-    if chosen_drink in user_drinks.drink_names:
-        #get drink id from original API data
-        chosen_drink_id = drink_id(chosen_drink, user_drinks.drink_info)
-        recipe = Recipe(chosen_drink_id) #access recipe API for chosen drink using drink id
-        print(recipe)
-    else:
-        print('Sorry, that drink is not on the list.')
-else:
-    print('Sorry, we don\'t know that spirit.')
+def get_drink_list(spirit):
+    drink_list = Drinks()
+    drink_list.populate_drinklist(spirit)
+    return drink_list
 
-print("-----------------------------------------------------")
+def get_confirmation() -> bool:
+    user_input = input("Confirm? y/n: ")
+    return user_input.strip().lower() in ("yes", "y")
 
+def set_user_drink(drink_list: Drinks()):
+    confirm = False
+    while not confirm:
+        user_index = input("Enter the number of the drink you'd like to see the recipe of: ")
+        user_drink = None
+        while user_drink is None:
+            user_drink = drink_list.pick_drink_with_index(int(user_index))
+            print(f"You picked {user_drink}.")
+            confirm = get_confirmation()
+    return user_drink
 
+def print_recipe(drink, drink_list):
+    drink_id = drink_list.get_drink_id(drink)
+    print(Recipe(drink_id))
 
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+if __name__ == "__main__":
+    print("\n---Welcome to your personal drink generator---\n")
+    print_list_of_spirits()
+
+    spirit = set_user_spirit()
+    drink_list = get_drink_list(spirit)
+    drink_list.print_drinklist()
+    user_drink = set_user_drink(drink_list)
+
+    print_recipe(user_drink, drink_list)
 
