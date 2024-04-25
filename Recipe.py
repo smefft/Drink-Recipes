2d1d791ec647c967e958f27d19f8101391154766
Sandra Effert <sandra.m.effert@gmail.com>
2024-04-25 23:25:35 +0200

    Implement RecipePage, FIX frame tracking

--- Recipe.py
+++ Recipe.py
@@ -1,22 +1,26 @@
 import requests
 
+
 class Recipe:
+
     def __init__(self, id):
         self.id = id
         self._recipe: dict[str, str] = self._set_recipe()
         self.drink_name: str = self._set_drinkname()
         self.ingredients: list[tuple[str, str]] = self._set_ingredients()
         self.instructions: list[str] = self._set_instructions()
-        
+
     def _set_recipe(self) -> dict[str, str]:
-        response = requests.get(f'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={self.id}')
-        token:dict = response.json()
+        response = requests.get(
+            f'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={self.id}'
+        )
+        token: dict = response.json()
         recipe = token['drinks'][0]
         return recipe
-    
+
     def _set_drinkname(self) -> str:
         return self._recipe["strDrink"]
-    
+
     def _set_ingredients(self) -> list[tuple[str, str]]:
         ingredients = []
         # in json file, ingredients are named strIngredient1, strIngredient2, etc
@@ -31,23 +35,34 @@ class Recipe:
     def _set_instructions(self) -> str:
         # in json file, instructions are inconsistent, sometimes numbering the steps, sometimes not.
         # that's why there is no formatting done bc it creates inconsistencies when printing
-        return self._recipe["strInstructions"]
-    
+        instructions = self._recipe["strInstructions"]
+        instructions = instructions.split(". ")
+        return "\n".join(instructions)
+
     def get_drinkname(self) -> str:
         return self.drink_name
-    
+
     def get_ingredients(self, as_list: bool = False) -> str or list:
         return self.ingredients if as_list else self._str_ingredients()
-    
+
     def get_instructions(self) -> str:
         return self.instructions
+
+    def get_picture(self):
+        return self._recipe["strDrinkThumb"]
+    
+    def get_recipe(self):
+        return self._recipe
 
     def _str_ingredients(self) -> str:
         ingredients = []
         for (ingredient_name, measurement) in self.ingredients:
-            ingredients.append(f"- {measurement} {ingredient_name}")
+            if measurement:
+                ingredients.append(f"- {measurement.strip()} {ingredient_name.strip()}")
+            else:
+                ingredients.append(f"- {ingredient_name.strip()}")
         return "\n".join(ingredients)
-    
+
     def __str__(self) -> str:
         header = f"\n---- Recipe for {self.get_drinkname()}: ----"
         ingredients = "\n".join(("Ingredients:", self.get_ingredients()))
--- view.py
+++ view.py
@@ -2,66 +2,73 @@ from tkinter import *
 from functools import partial
 from Spirits import SPIRITS
 import Drinks
+from Recipe import Recipe
 
 # TODO 1 compartmentalize
 # TODO 2 make scrollable
 # TODO 4 make back and quit buttons in different spots
 # TODO 5 connect drinks to recipes (make recipe screen)
 
+
 class DrinkListApp(Tk):
+
     def __init__(self):
         Tk.__init__(self)
         self.geometry('3000x1600')
 
         self.main_container = Frame(self)
-        self.main_container.pack(side = "top", fill = "both", expand = True)
+        self.main_container.pack(side="top", fill="both", expand=True)
 
-        self.main_container.grid_rowconfigure(0, weight = 1)
-        self.main_container.grid_columnconfigure(0, weight = 1)
+        self.main_container.grid_rowconfigure(0, weight=1)
+        self.main_container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
+        self.frames: list[Frame] = []
 
-        for F in (SpiritsPage,):
-            frame = F(self.main_container, self)
-            self.frames[F] = frame
-            frame.grid(row = 0, column = 0, sticky ="nsew")
-        
-        self.show_frame(SpiritsPage)
+        self.show_next_frame(SpiritsPage)
+    
+    def add_frame(self, frame):
+        self.frames.append(frame)
+    
+    def remove_last_frame(self):
+        del self.frames[-1]
 
-    def show_frame(self, frame_class, spirit=None) -> None:
-        if frame_class in self.frames:
-            frame: Frame = self.frames[frame_class]
-        elif frame_class == DrinkListPage:
-            frame: Frame = DrinkListPage(self.main_container, self, spirit)
-            frame.grid(row = 0, column = 0, sticky ="nsew")
-        else:
-            raise NotImplementedError
+    def show_next_frame(self, page, *args) -> None:
+        frame = page(self.main_container, self, *args)
+        frame.grid(row=0, column=0, sticky="nsew")
+        self.add_frame(frame)
         frame.tkraise()
+    
+    def show_previous_frame(self) -> None:
+        self.remove_last_frame()
+        frame = self.frames[-1]
+        frame.tkraise()
+
 
 class SubFrame(Frame):
     def __init__(self, parent: Frame, controller: Tk):
         Frame.__init__(self, parent)
-        self.controller = controller
-    
-    def _back_button(self, back_to_frame: Frame):
+        self.parent = parent
+        self.controller: Tk = controller
+
+    def _back_button(self, **kwargs):
         back_button = Button(
-            self, 
-            text="Back",
-            command=partial(self.controller.show_frame, back_to_frame), 
+            self,
+            text="< Back",
+            command=self.controller.show_previous_frame,
             foreground="blue",
         )
-        back_button.pack(pady=4)
-    
+        back_button.pack(anchor="nw", side="top", pady=4, padx=4)
+
     def _quit_button(self):
         quit_button = Button(self, text="Quit", command=self.controller.destroy, foreground="red")
-        quit_button.pack(pady=4)
+        quit_button.pack(anchor="ne", side="top", pady=4, padx=4)
 
 class SpiritsPage(SubFrame):
     def __init__(self, parent: Frame, controller: Tk):
         super().__init__(parent, controller)
         label = Label(self, text="Pick your Spirit: ", fg="#000000")
         label.pack(pady=4)
-        
+
         self._quit_button()
 
         for spirit in SPIRITS:
@@ -70,34 +77,108 @@ class SpiritsPage(SubFrame):
                 text=spirit.capitalize(),
                 width=10,
                 height=2,
-                command=partial(controller.show_frame, DrinkListPage, spirit),
+                command=partial(controller.show_next_frame, DrinkListPage, spirit),
             )
             button.pack(pady=4)
 
+
 class DrinkListPage(SubFrame):
+    @property
+    def drinklist(self):
+        return self._drinklist
+    @drinklist.setter
+    def drinklist(self, value):
+        assert isinstance(value, Drinks.DrinkList)
+        self._drinklist = value
+
     def __init__(self, parent: Frame, controller: Tk, spirit_name: str):
         super().__init__(parent, controller)
         self.spirit_name = spirit_name.capitalize()
-        self.label = Label(self, text=f"Drinks with {self.spirit_name}:", fg="#000000")
-        self.drinklist = self._get_drinklist()
+        self.drinklist = Drinks.DrinkList(self.spirit_name)
+        self.drinknames = self._get_drinknames()
 
-        self._back_button(SpiritsPage)
+        self._header()
+        self._back_button()
         self._quit_button()
         self._drink_buttons()
 
-    def _get_drinklist(self) -> dict:
-        return Drinks.DrinkList(self.spirit_name).get_drinklist()
+    def _header(self):
+        self.label = Label(self,
+                           text=f"Drinks with {self.spirit_name}:",
+                           fg="#000000")
+        self.label.pack(pady=4)
+
+    def _get_drinknames(self) -> dict:
+        return self.drinklist.get_drinklist()
     
+    def _get_drinkid(self, drinkname):
+        return self.drinklist.get_drink_id(drinkname)
+
     def _drink_buttons(self):
-        drinkname_lengths = [len(drinkname) for drinkname in self.drinklist]
+        drinkname_lengths = [len(drinkname) for drinkname in self.drinknames]
         button_width = max(drinkname_lengths) + 2
-        for drink_name in self.drinklist:
+        for drinkname in self.drinknames:
+            assert isinstance(drinkname, str)
+            drink_id = self._get_drinkid(drinkname)
             button = Button(
                 self,
-                text=drink_name.capitalize(),
+                text=drinkname.title(),
                 width=button_width,
+                command=partial(
+                    self.controller.show_next_frame, RecipePage, drink_id)
             )
             button.pack(pady=4)
+
+
+class RecipePage(SubFrame):
+
+    @property
+    def recipe(self):
+        recipe = self._recipe
+        assert isinstance(recipe, Recipe)
+        return recipe
+    @recipe.setter
+    def recipe(self, value):
+        assert isinstance(value, Recipe)
+        self._recipe = value
+
+    def __init__(self, parent: Frame, controller: Tk, drink_id: str):
+        super().__init__(parent, controller)
+        self.drink_id = drink_id
+        self.recipe = self._get_recipe()
+        self._back_button()
+        self._quit_button()
+        self.display_recipe()
+
+    def display_recipe(self):
+        self._header()
+        self._ingredients()
+        self._instructions()
+        # self._photo()
+    
+    def _photo(self):
+        picture = self.recipe.get_picture()
+        if picture:
+            recipe_picture = PhotoImage(master=self, data=picture)
+
+    def _header(self):
+        drinkname = self.recipe.get_drinkname()
+        # TODO make bigger
+        recipe_header = Label(self, text=drinkname.title(), fg='#000000')
+        recipe_header.pack(pady=4)
+    
+    def _ingredients(self):
+        ingredients = self.recipe.get_ingredients()
+        recipe_ingredients = Label(self, text=ingredients, fg='#000000', justify='left')
+        recipe_ingredients.pack(pady=4)
+    
+    def _instructions(self):
+        instructions = self.recipe.get_instructions()
+        recipe_instructions = Label(self, text=instructions, fg='#000000', justify='left')
+        recipe_instructions.pack(pady=4)
+
+    def _get_recipe(self):
+        return Recipe(self.drink_id)
 
 app = DrinkListApp()
 app.mainloop()
