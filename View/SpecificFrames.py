f0bc1e0f7292f7d8593882d9dffe64671577bb3a
Sandra Effert <sandra.m.effert@gmail.com>
2024-11-03 19:53:31 -0500

    ADD DrinkListFrame

--- View/SpecificFrames.py
+++ View/SpecificFrames.py
@@ -1,20 +1,5 @@
 from functools import partial
-from Bases import Controller, InnerFrame
-
-class DrinkListApp:
-    def __init__(self):
-        self.width = 1300
-        self.height = 700
-        self.title = "DrinkChooser"
-        self.controller= Controller(self.width, self.height, self.title)
-
-        first_frame = SpiritsFrame(self.controller, ["spirit2", "spirit1"]).base_frame
-        self.controller.add_frame(first_frame)
-        self.controller.show_next_frame()
-
-    def run(self):
-        """Starts the app"""
-        self.controller.mainloop()
+from View.Bases import Controller, InnerFrame # Import from View package
 
 class SpiritsFrame(InnerFrame):
     """Sets up content of the inner frame
@@ -23,16 +8,32 @@ class SpiritsFrame(InnerFrame):
     def __init__(self, controller: Controller, spirits: list[str]):
         super().__init__(controller)
         self.add_header("Pick your Spirit: ")
-        for spirit in spirits:
+        for count, spirit in enumerate(spirits, 1):
             self.add_button(
                 text=spirit.capitalize(),
                 width=10,
                 height=2,
-                command=partial(controller.show_next_frame, DrinkListFrame, spirit),)
+                command=partial(controller.app.get_drinklist, spirit),
+                count=count)
+
 
 class DrinkListFrame(InnerFrame):
-    def __init__(self, controller, spirit_name: str):
-        super().__init__(controller)
 
-app = DrinkListApp()
-app.run()
+    def __init__(self, controller, spirit, drinklist):
+        super().__init__(controller, scrollable=True)
+        self.add_header(f"Drinks with {spirit.capitalize()}: ")
+
+        drink_names = drinklist.keys()
+        button_width = max([len(drinkname) for drinkname in drink_names]) + 2
+        for count, (name, drink_id) in enumerate(drinklist.items(), 1):
+            self.add_button(
+                text=name.title(),
+                width=button_width,
+                height=1,
+                command=partial(controller.app.get_recipe, drink_id),
+                count=count)
+
+class RecipeFrame(InnerFrame):
+
+    def __init__(self, controller, recipe):
+        super().__init__(controller)
